d629166c5015e427587eb24afe874b265bbfa80d
Tiago Alves <tiago.farias@dcomp.ufs.br>
2020-06-17 21:54:01 -0300

    add squery

---
+++ All Files/jquery/JQueryInformacoesUteis.js
@@ -0,0 +1,35 @@
+/*
+JQuery: é uma biblioteca do javascript, usada para manipular a DOM.
+- Ajuda na busca de elementos e alteração da DOM.
+- Trabalha com manipulação de eventos, animações, Ajax...
+- Pensado para trabalhar com conjunto de elementos
+- Usa seletores CSS
+- Encadeamento de chamadas chaining (consegui chamar varias funções em uma única linha. "$('div').hide(5000).show(5000).fadeOut(5000)")
+- É de fácil extensão usando plugins (consegui criar plugins facilmente)
+- Suporte múltiplos Browsers
+- Só tem pontos positivos? Não!
+
+É referênciado com o símbolo $ ou JQuery.
+
+$()
+    Função.
+
+    Filtro :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
